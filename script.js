a5337b9a5fc9a629c4bb4cafd365e5cb5ab746e3
Thomas NG <hkdjyu@gmail.com>
2026-05-08 14:43:43 +0800

    First commit

---
+++ script.js
@@ -0,0 +1,6 @@
+function changeText() {
+    const msg = document.getElementById("message");
+    msg.innerHTML = "🎉 JavaScript 成功啟動了互動功能！";
+    msg.style.color = "red";
+    msg.style.fontWeight = "bold";
+}
